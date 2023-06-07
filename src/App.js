69b25f94abb06f4d74c90a07a3f300a919fa9f32
GabrielNog <gabrielnog.nda@gmail.com>
2023-06-07 14:01:24 -0300

    A clean initial structure

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Hello, Word!
+        </p>
+        <a>
+          
+        Hello there!
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
